2125ccb814ac32fb5f5ebd9f05fbdd85e0b235e5
Wompa-Stompa <chavothewonderdog@gmail.com>
2022-06-04 07:46:24 -0500

    roughstylehomepage

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '2560',
+    },
     extend: {},
   },
   plugins: [],
